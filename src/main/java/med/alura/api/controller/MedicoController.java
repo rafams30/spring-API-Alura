ffe7c2782cdf33dbc695efa830648c9bf8cc2e28
Rafael Martines <rafams_22@hotmail.com>
2024-04-22 17:04:03 -0300

    Interfaces Repository

--- src/main/java/med/alura/api/controller/MedicoController.java
+++ src/main/java/med/alura/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.alura.api.controller;
 
+import med.alura.api.endereco.Endereco;
 import med.alura.api.medico.DadosCadastroMedico;
+import med.alura.api.medico.Medico;
+import med.alura.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+       repository.save(new Medico(dados));
     }
 }
