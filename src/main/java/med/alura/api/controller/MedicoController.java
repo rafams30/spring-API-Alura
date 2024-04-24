0ee4176d38faf53455fd66d23c176b7a2da8e162
Rafael Martines <rafams_22@hotmail.com>
2024-04-24 08:24:12 -0300

    Produção de dados na API

--- src/main/java/med/alura/api/controller/MedicoController.java
+++ src/main/java/med/alura/api/controller/MedicoController.java
@@ -3,13 +3,11 @@ package med.alura.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.alura.api.medico.DadosCadastroMedico;
+import med.alura.api.medico.DadosListagemMedico;
 import med.alura.api.medico.Medico;
 import med.alura.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,8 +24,8 @@ public class MedicoController {
        repository.save(new Medico(dados));
     }
 
-    @
-    public List<Medico> listar(){
-
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
