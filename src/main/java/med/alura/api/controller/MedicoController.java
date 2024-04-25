c49fd753bcd4366fd8088ae8f203853a3d41cdf7
Rafael Martines <rafams_22@hotmail.com>
2024-04-25 14:23:39 -0300

    Detalhando dados na API

--- src/main/java/med/alura/api/controller/MedicoController.java
+++ src/main/java/med/alura/api/controller/MedicoController.java
@@ -53,4 +53,10 @@ public class MedicoController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
 }
