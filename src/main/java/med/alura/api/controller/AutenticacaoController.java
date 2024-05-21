5bd87c0b9dfb54b626ed32cbe86cba2420781d5a
Rafael Martines <rafams_22@hotmail.com>
2024-05-20 21:21:04 -0300

    Gerando tokens JWT

--- src/main/java/med/alura/api/controller/AutenticacaoController.java
+++ src/main/java/med/alura/api/controller/AutenticacaoController.java
@@ -2,6 +2,8 @@ package med.alura.api.controller;
 
 import jakarta.validation.Valid;
 import med.alura.api.domain.usuario.DadosAutenticacao;
+import med.alura.api.domain.usuario.Usuario;
+import med.alura.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +20,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authenticaon = manager.authenticate(token);
+        var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok("");
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
